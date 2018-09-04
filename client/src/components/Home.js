86abcedf7c6492bf21a0c09bf8d2ae3d481127ab
Jin827 <25231717+Jin827@users.noreply.github.com>
2018-09-04 12:48:20 +0900

    create reserve component

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,8 +1,9 @@
 import React from 'react';
-/* --- Components --- */
-import HomeMain from './Home.main';
 /* --- Shared --- */
 import Nav from '../shared/nav';
+/* --- Components --- */
+import HomeMain from './home.main';
+import Reserve from './home.reserve';
 
 const Home = () => (
   <div>
@@ -10,13 +11,14 @@ const Home = () => (
       <Nav />
       <div className="header-text--box">
         <h2>
-          <span className="f-regular f-en lh-3">NO MSG!</span>
+          <span className="f-regular f-en lh-3">NO MSG&#33;</span>
           <br />
-          오늘도 열심히 일하는 당신에게 착한 가격의 집밥을 선물하세요.
+          오늘도 열심히 일하는 당신에게 착한 가격의 집밥을 선물하세요&#46;
         </h2>
       </div>
     </header>
     <HomeMain />
+    <Reserve />
   </div>
 );
 
---
+++ client/src/components/home.reserve.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Reserve extends React.Component {
+  render() {
+    return (
+      <div className="tc white reserve-container">
+        <h3 className="white f-xl mb2">Reservation</h3>
+        <p>기업체 각종 행사, 모임 단체 식사 주문받습니다&#46;</p>
+      </div>
+    );
+  }
+}
+
+export default Reserve;
