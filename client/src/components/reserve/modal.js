9ce13c7281c649581400a29b3f66109ef78d1660
Jin827 <25231717+Jin827@users.noreply.github.com>
2018-09-12 16:05:36 +0900

    update react hot loadder to fix unsafe legacy lifecycles warning

--- client/src/components/reserve/modal.js
+++ client/src/components/reserve/modal.js
@@ -19,7 +19,7 @@ const SimpleModal = ({ show, handleClose, handleChange }) => (
         예약 상담과 확정을 위해 24시간 이내로 연락을 드리겠습니다. 만약 연락을
         못받으시면, 유청으로 연락주시길 바랍니다.
       </Typography>
-      <ReserveFormsWrapped handleChange={handleChange} />
+      {/* <ReserveFormsWrapped handleChange={handleChange} /> */}
     </div>
   </Modal>
 );
