a92ce8a0f4e07af25867a6c8d493cabdbd8a313c
Jin827 <25231717+Jin827@users.noreply.github.com>
2018-10-16 14:00:35 +0900

    <switchReserve /> testing

--- client/src/__tests__/features/reserve/components/switchReserve.test.js
+++ client/src/__tests__/features/reserve/components/switchReserve.test.js
@@ -6,10 +6,12 @@ describe('<switchReserve />', () => {
   it('render reserve form when api request not made', () => {
     const wrapper = shallow(<SwitchReserve apiRequest="" />);
     expect(wrapper).toMatchSnapshot();
+    expect(wrapper.find('WithStyles(ReserveForm)').exists()).toBe(true);
   });
 
   it('render reserve request resolved text when api request is made', () => {
     const wrapper = shallow(<SwitchReserve apiRequest="success" />);
     expect(wrapper).toMatchSnapshot();
+    expect(wrapper.find('ReserveResolvedText').exists()).toBe(true);
   });
 });
