14974427930614b4dd043e4b46cde23153805b59
Jin827 <25231717+Jin827@users.noreply.github.com>
2018-11-07 18:35:48 +0900

    header style

--- client/src/features/home/header.js
+++ client/src/features/home/header.js
@@ -7,10 +7,11 @@ const Header = () => (
     <Nav />
     <div className="header-text--box">
       <h2>
-        <span className="f-xl f-en lh-3">NO MSG&#33;</span>
+        <span className="f-xxl f-en lh-3">NO MSG&#33;</span>
         <br />
         오늘도 열심히 일한 당신에게 <br />
-        당신만을 위한 착한 가격의 집밥을 선물하세요&#46;
+        당신만을 위한 <span className="point">착한 가격의 집밥을</span>{' '}
+        선물하세요&#46;
       </h2>
     </div>
   </header>
