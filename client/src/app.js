2270b38cbccfea3ba35a2802f58b6502e923a28a
Jin827 <25231717+Jin827@users.noreply.github.com>
2018-11-08 16:39:44 +0900

    add favicons

--- client/src/app.js
+++ client/src/app.js
@@ -18,26 +18,17 @@ const App = props => {
       <Helmet>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>###</title>
-        <meta property="og:title" content="###" />
-        <meta property="og:description" content="###" />
+        <title>위탁급식/식당 유청</title>
+        <meta property="og:title" content="위탁급식/식당 유청" />
+        <meta
+          property="og:description"
+          content="착한가격의 집밥, 위탁 급식, 뷔폐 식당, 기업체 각종 행사/ 모임 단체 식사, 경주 전지역 배달, No MSG, 신선한 지역 농산물"
+        />
         <meta property="og:type" content="###" />
         <meta property="og:url" content="###" />
-        <meta property="og:image" content="###" />
+        <meta property="og:image" content="./../assets/img/header.jpg" />
         <link type="text/plain" rel="author" href="http://domain/humans.txt" />
         <link type="text/plain" rel="author" href="http://domain/robots.txt" />
-        <link
-          href="https://fonts.googleapis.com/css?family=Nanum+Gothic|Notable"
-          rel="stylesheet"
-        />
-        <link
-          href="https://fonts.googleapis.com/css?family=Luckiest+Guy"
-          rel="stylesheet"
-        />
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
-        />
       </Helmet>
       {props.children}
       {!isHomepage ? <Nav /> : null}
