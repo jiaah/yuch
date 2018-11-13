c52cc07efacfbf3c364d6582fb33ec04d56ec3f3
Jin827 <25231717+Jin827@users.noreply.github.com>
2018-11-13 14:52:42 +0900

    reserve async action testing

--- client/src/actions/reserveAction.test.js
+++ client/src/actions/reserveAction.test.js
@@ -1,33 +1,48 @@
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
-import MockAdapter from 'axios-mock-adapter';
-import axios from 'axios';
+import moxios from 'moxios';
 // import mockAxios from 'axios';
 import * as types from './actionTypes';
 import * as mockData from '../__tests__/__mocks__/mockData';
-import { reserve, resetReserve } from './reserveAction';
+import * as actions from './reserveAction';
 
 // config store/ middleware & axios
 const middleware = [thunk];
 const mockStore = configureMockStore(middleware);
-const mockAxios = new MockAdapter(axios);
 
 test('should generate reset reserve info action', () => {
   const expectedAction = { type: types.RESET_RESERVE };
-  expect(resetReserve()).toEqual(expectedAction);
+  expect(actions.resetReserve()).toEqual(expectedAction);
 });
 
 describe('async reserve request actions', async () => {
-  // mockAxios.onPost('/api/reserve', mockRequest).reply(200);
-  // const expectedActions = [
-  //   {
-  //     type: types.RESERVE_REQUEST,
-  //     body: { reserveInfo: mockData.reserveUpdatedState },
-  //   },
-  //   { type: types.RESERVE_SUCCESS },
-  // ];
-  // const store = mockStore({});
-  // store.dispatch(reserve()).then(() => {
-  //   expect(store.getActions()).toEqual(expectedActions);
-  // });
+  beforeEach(() => {
+    moxios.install();
+  });
+
+  afterEach(() => {
+    moxios.uninstall();
+  });
+
+  it('creates RESERVE_SUCCESS after successfully sending reserve request', () => {
+    moxios.wait(() => {
+      const request = moxios.requests.mostRecent();
+      expect(request.config.method).toEqual('post');
+
+      request.respondWith({
+        status: 200,
+      });
+    });
+
+    const expectedActions = [
+      { type: types.RESERVE_REQUEST },
+      { type: types.RESERVE_SUCCESS },
+    ];
+
+    const store = mockStore({});
+
+    return store.dispatch(actions.reserve()).then(() => {
+      expect(store.getActions()).toEqual(expectedActions);
+    });
+  });
 });
