db35cf1c837f416fba4a7786b742ff9c13e1c703
Jin827 <25231717+Jin827@users.noreply.github.com>
2018-11-14 20:05:37 +0900

    add the right name files for routes and store

---
+++ client/store.js
@@ -0,0 +1,25 @@
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { routerMiddleware } from 'react-router-redux';
+import createHistory from 'history/createBrowserHistory';
+import Reducers from './src/reducers';
+
+const routMiddleware = routerMiddleware(createHistory());
+
+const Store = createStore(
+  Reducers,
+  composeWithDevTools(
+    applyMiddleware(
+      routMiddleware,
+      thunkMiddleware,
+      createLogger({
+        predicate: () => process.env.NODE_ENV === 'development',
+        collapsed: true,
+      }),
+    ),
+  ),
+);
+
+export default Store;
