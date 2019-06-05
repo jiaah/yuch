a107d97b359fcc4a2a04a5e9cc5a2c6dec477bb4
Jin827 <25231717+Jin827@users.noreply.github.com>
2019-06-05 15:58:38 +0900

    remove greeting user section in nav for admin

--- client/src/components/Nav/navContainer.js
+++ client/src/components/Nav/navContainer.js
@@ -32,13 +32,14 @@ class NavContainer extends Component {
             <img className="yuch-logo" src={logo} alt="logo" />
           </Link>
         </div>
-        {isLoggedIn && (
-          <p className="mr3 pb2 flex justify-end">
-            안녕하세요. &#8201;
-            <span className="b">{userName}</span>
-            &#8201;님,
-          </p>
-        )}
+        {isLoggedIn &&
+          userName !== 'yuchung' && (
+            <p className="mr3 pb2 flex justify-end">
+              안녕하세요. &#8201;
+              <span className="b">{userName}</span>
+              &#8201;님,
+            </p>
+          )}
         <NavMenu
           routerLocation={routerLocation}
           isLoggedIn={isLoggedIn}
