818f01a26920fc88304d4e539f8b4c41f2d4860d
Jin827 <25231717+Jin827@users.noreply.github.com>
2019-06-20 16:11:43 +0900

    add condition to render user account list

--- client/src/components/auth/admin/userTable.js
+++ client/src/components/auth/admin/userTable.js
@@ -60,66 +60,67 @@ const UserTable = ({
             onRequestSort={handleRequestSort}
           />
           <TableBody>
-            {stableSort(rows, getSorting(order, orderBy))
-              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              .map((row, index) => {
-                const labelId = `enhanced-table-checkbox-${index}`;
-                const bankAccountInfo =
-                  row.bankAccountId === 1 ? '김귀자 농협' : '이상환 농협';
-                return (
-                  <TableRow
-                    hover
-                    onClick={() => handleTableRowClick(row.username)}
-                    role="checkbox"
-                    aria-checked={selected === row.username}
-                    tabIndex={-1}
-                    key={row.username}
-                    selected={selected === row.username}
-                  >
-                    <TableCell padding="checkbox">
-                      <IconButton
-                        name="edit"
-                        width="19"
-                        height="19"
-                        viewBox="0 0 24 24"
-                        handleClick={e =>
-                          handleEditUserBtnClick(e, row.username)
-                        }
-                      />
-                    </TableCell>
-                    <TableCell
-                      component="th"
-                      id={labelId}
-                      scope="row"
-                      padding="none"
-                      className={resize}
+            {rows.length !== 0 &&
+              stableSort(rows, getSorting(order, orderBy))
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((row, index) => {
+                  const labelId = `enhanced-table-checkbox-${index}`;
+                  const bankAccountInfo =
+                    row.bankAccountId === 1 ? '김귀자 농협' : '이상환 농협';
+                  return (
+                    <TableRow
+                      hover
+                      onClick={() => handleTableRowClick(row.username)}
+                      role="checkbox"
+                      aria-checked={selected === row.username}
+                      tabIndex={-1}
+                      key={row.username}
+                      selected={selected === row.username}
                     >
-                      {row.companyName}
-                    </TableCell>
-                    <TableCell align="right" className={resize}>
-                      {row.username}
-                    </TableCell>
-                    <TableCell align="right" className={resize}>
-                      {row.contactNo}
-                    </TableCell>
-                    <TableCell align="right" className={resize}>
-                      {row.email}
-                    </TableCell>
-                    <TableCell align="right" className={resize}>
-                      {row.mealPrice}
-                    </TableCell>
-                    <TableCell align="right" className={resize}>
-                      {row.lunchQty}
-                    </TableCell>
-                    <TableCell align="right" className={resize}>
-                      {row.dinnerQty}
-                    </TableCell>
-                    <TableCell align="right" className={resize}>
-                      {bankAccountInfo}
-                    </TableCell>
-                  </TableRow>
-                );
-              })}
+                      <TableCell padding="checkbox">
+                        <IconButton
+                          name="edit"
+                          width="19"
+                          height="19"
+                          viewBox="0 0 24 24"
+                          handleClick={e =>
+                            handleEditUserBtnClick(e, row.username)
+                          }
+                        />
+                      </TableCell>
+                      <TableCell
+                        component="th"
+                        id={labelId}
+                        scope="row"
+                        padding="none"
+                        className={resize}
+                      >
+                        {row.companyName}
+                      </TableCell>
+                      <TableCell align="right" className={resize}>
+                        {row.username}
+                      </TableCell>
+                      <TableCell align="right" className={resize}>
+                        {row.contactNo}
+                      </TableCell>
+                      <TableCell align="right" className={resize}>
+                        {row.email}
+                      </TableCell>
+                      <TableCell align="right" className={resize}>
+                        {row.mealPrice}
+                      </TableCell>
+                      <TableCell align="right" className={resize}>
+                        {row.lunchQty}
+                      </TableCell>
+                      <TableCell align="right" className={resize}>
+                        {row.dinnerQty}
+                      </TableCell>
+                      <TableCell align="right" className={resize}>
+                        {bankAccountInfo}
+                      </TableCell>
+                    </TableRow>
+                  );
+                })}
             {emptyRows > 0 && (
               <TableRow style={{ height: 49 * emptyRows }}>
                 <TableCell colSpan={6} />
