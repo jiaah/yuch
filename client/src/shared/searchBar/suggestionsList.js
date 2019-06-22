c26834d1deba5bb6515ae2a1e729ca4758e951ee
Jin827 <25231717+Jin827@users.noreply.github.com>
2019-06-22 14:57:11 +0900

    searchBar autoComplete style

--- client/src/shared/searchBar/suggestionsList.js
+++ client/src/shared/searchBar/suggestionsList.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
 const SuggestionsList = ({ suggestions, suggestionSelected }) => (
-  <ul className="pa3">
+  <ul>
     {suggestions.map(u => (
-      <li key={u.id} onClick={() => suggestionSelected(u.companyName)}>
-        {u.companyName}
+      <li
+        key={u.id}
+        className="autoComplete-item"
+        onClick={() => suggestionSelected(u.companyName)}
+      >
+        <div className="pa3">{u.companyName}</div>
       </li>
     ))}
   </ul>
