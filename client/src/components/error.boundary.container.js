8c4ccc7d2e000d39d9d31f19fb69d0e07c41f1e6
Jin827 <25231717+Jin827@users.noreply.github.com>
2019-08-14 15:29:11 +0900

    modify error pages UI

--- client/src/components/error.boundary.container.js
+++ client/src/components/error.boundary.container.js
@@ -6,46 +6,32 @@ class ErrorBoundary extends Component {
     super(props);
 
     this.state = {
-      hasError: false,
+      error: null,
+      errorInfo: null,
     };
   }
 
-  static getDerivedStateFromError(error) {
-    // Update state so the next render will show the fallback UI.
-    return { hasError: true };
-  }
-
   componentDidCatch(error, errorInfo) {
-    // You can also log the error to an error reporting service
-    logErrorToMyService(error, errorInfo);
+    this.setState({
+      error,
+      errorInfo,
+    });
   }
 
   render() {
-    console.log('this.state.hasError: ', this.state.hasError);
-    if (this.state.hasError) {
+    console.log(this.state.error && this.state.error.toString());
+    console.log(this.state.errorInfo.componentStack);
+    if (this.state.errorInfo) {
       return (
         <div className="error--container">
-          <a
-            href="https://www.clipart.email/download/361468.html"
-            title="Image from clipart.email"
-          >
-            <img
-              src="https://cdn.clipart.email/06b868cb5a845be9120c2f36783a6d83_error-vector-clip-art-royalty-free-13922-error-clipart-vector-_200-194.jpeg"
-              width="350"
-              alt="Human Error Clipart"
-            />
-          </a>
-          <div className="tc f-regular lh-1 error--p">
-            <div className="f-xl lh-1 error--p-1">
-              <p className="main-blue">Oops !</p>
-              <p className="main-blue">시스템 오류입니다.</p>
+          <div id="notfound">
+            <div className="notfound">
+              <div className="notfound-404">
+                <h1>Oops!</h1>
+                <h2>시스템 오류입니다.</h2>
+              </div>
+              <a href="/">Go TO Homepage</a>
             </div>
-            <div className="mt4 fw3 error--p-2">
-              <p>브라우저를 새로고침하거나 나중에 다시 시도해주세요.</p>
-            </div>
-            <button type="button" to="/">
-              메인페이지로 바로가기
-            </button>
           </div>
         </div>
       );
--- client/src/components/noMatch.js
+++ client/src/components/noMatch.js
@@ -1,16 +1,14 @@
 import React from 'react';
 
 const FourOhFour = () => (
-  <div className="fourohfour flex center-center flex-column-m tc">
-    <h2 className="f-en mb3">oh no...</h2>
-    <p>페이지를 찾을수가 없습니다.</p>
-    <h2 className="f-en mh1">Error 404</h2>
-    <p>
-      죄송합니다. 유효한 페이지 링크인지 확인해주시거나 브라우저 새로고침을
-      해주세요.
-    </p>
-    <br />
-    <p>웹사이트 사용에 문제가 있으시면 유청으로 문의해주세요.</p>
+  <div id="notfound">
+    <div className="notfound">
+      <div className="notfound-404">
+        <h1>Oops!</h1>
+        <h2>404 - 페이지를 찾을수가 없습니다.</h2>
+      </div>
+      <a href="/">Go TO Homepage</a>
+    </div>
   </div>
 );
 
