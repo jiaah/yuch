e9c25aef72730d00e49f33088704e929841b93b6
Jin827 <25231717+Jin827@users.noreply.github.com>
2019-08-21 11:35:57 +0900

    upgrade npm packages

--- cypress/e2e/auth.js
+++ cypress/e2e/auth.js
@@ -2,9 +2,13 @@
 import faker from 'faker';
 
 describe('user', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:9080');
+  });
+
   it('can visit login app', () => {
     cy.visit('/login');
   });
 
-  it('protected route', () => {});
+  it('blocks protected routes', () => {});
 });
