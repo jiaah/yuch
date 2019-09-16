b377f2c79f54d001ac349f16e1d4a8afcb4cfc31
Jin827 <25231717+Jin827@users.noreply.github.com>
2019-09-16 14:36:58 +0900

    feat: display none for mealPrice when reserveDate = thisMonth

--- client/src/components/admin/rates/ratesTableRow.js
+++ client/src/components/admin/rates/ratesTableRow.js
@@ -4,6 +4,7 @@ import TableCell from '@material-ui/core/TableCell';
 import TableRow from '@material-ui/core/TableRow';
 /* --- Components --- */
 import IconButton from '../../../shared/form/iconButton';
+import { thisMonth } from '../../../helpers/moment';
 
 const styles = theme => ({
   resize: {
@@ -21,48 +22,53 @@ const RatesTableRow = ({
   selected,
   handleTableRowClick,
   handleEditUserBtnClick,
-}) => (
-  <React.Fragment>
-    <TableRow
-      key={row.userId}
-      hover
-      role="checkbox"
-      tabIndex={-1}
-      aria-checked={selected === row.userId}
-      selected={
-        selectedSearchItem === row.companyName || selected === row.userId
-      }
-      onClick={() => handleTableRowClick(row.userId)}
-    >
-      <TableCell padding="checkbox">
-        <IconButton
-          name="edit"
-          width="19"
-          height="19"
-          viewBox="0 0 24 24"
-          handleClick={e => handleEditUserBtnClick(e, row.userId)}
-        />
-      </TableCell>
-      <TableCell
-        component="th"
-        id={labelId}
-        scope="row"
-        padding="none"
-        className={resize}
+}) => {
+  // if change mealPrice of this month, display the new price value in only reserveData column.
+  const newMealPrice = row.reserveDate === thisMonth ? '' : row.mealPrice;
+
+  return (
+    <React.Fragment>
+      <TableRow
+        key={row.userId}
+        hover
+        role="checkbox"
+        tabIndex={-1}
+        aria-checked={selected === row.userId}
+        selected={
+          selectedSearchItem === row.companyName || selected === row.userId
+        }
+        onClick={() => handleTableRowClick(row.userId)}
       >
-        {row.companyName}
-      </TableCell>
-      <TableCell align="right" className={resize}>
-        {row.mealPrice}
-      </TableCell>
-      <TableCell align="right" className={resize}>
-        {row.reservePrice}
-      </TableCell>
-      <TableCell align="right" className={resize}>
-        {row.reserveDate}
-      </TableCell>
-    </TableRow>
-  </React.Fragment>
-);
+        <TableCell padding="checkbox">
+          <IconButton
+            name="edit"
+            width="19"
+            height="19"
+            viewBox="0 0 24 24"
+            handleClick={e => handleEditUserBtnClick(e, row.userId)}
+          />
+        </TableCell>
+        <TableCell
+          component="th"
+          id={labelId}
+          scope="row"
+          padding="none"
+          className={resize}
+        >
+          {row.companyName}
+        </TableCell>
+        <TableCell align="right" className={resize}>
+          {newMealPrice}
+        </TableCell>
+        <TableCell align="right" className={resize}>
+          {row.reservePrice}
+        </TableCell>
+        <TableCell align="right" className={resize}>
+          {row.reserveDate}
+        </TableCell>
+      </TableRow>
+    </React.Fragment>
+  );
+};
 
 export default withStyles(styles)(RatesTableRow);
