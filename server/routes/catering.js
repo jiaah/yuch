12cd0ae09916392fa4e7ef5e6ee5976a03e7a157
draconids <draconids@nate.com>
2019-09-16 17:17:46 +0900

    fix: unblock startedAt limit with join date

--- server/routes/catering.js
+++ server/routes/catering.js
@@ -5,6 +5,17 @@ const validation = require('../lib/validation');
 const cateringController = require('../controllers/catering');
 
 module.exports = () => {
+  router.get(
+    '/user/{id}',
+    onlyLoggedIn,
+    [
+      check('date')
+        .matches(/^[0-9]{4}\/[0-9]{2}\/[0-9]{2}$/)
+        .isISO8601(),
+    ],
+    cateringController.getLists,
+  );
+
   /* --- Login --- */
   router.get(
     '/users',
@@ -13,13 +24,6 @@ module.exports = () => {
     validation,
     cateringController.getLists,
   );
-
-  // router.post(
-  //   '/user/{id}',
-  //   onlyLoggedIn,
-  //   [check('reservePrice').isNumeric()],
-  //   cateringController.loginUser,
-  // );
 
   // router.patch(
   //   '/user/{id}',
