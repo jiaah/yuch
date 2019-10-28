5df8010294f57a5cfd809c78488c7339caffeeed
Jin827 <25231717+Jin827@users.noreply.github.com>
2019-10-28 13:30:57 +0900

    server: create routes table

--- server/controllers/delivery.js
+++ server/controllers/delivery.js
@@ -2,20 +2,20 @@ const knex = require('../database');
 
 exports.createRoute = (req, res) => {
   const { route } = req.body;
-  knex('delivery')
+  knex('routes')
     .insert({ route })
     .then(() => res.status(200).json())
     .catch(err => res.status(500).json(err));
 };
 
 exports.getRoutes = (req, res) =>
-  knex('delivery')
+  knex('routes')
     .select('*')
     .then(routes => res.status(200).json(routes))
     .catch(err => res.status(500).json(err));
 
 exports.deleteRoute = (req, res) => {
-  knex('delivery')
+  knex('routes')
     .where({ id: req.params.id })
     .first()
     .del()
---
+++ server/database/migrations/20191028131609_routes.js
@@ -0,0 +1,17 @@
+exports.up = knex =>
+  knex.schema
+    .raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
+    .createTable('routes', table => {
+      table
+        .uuid('id')
+        .primary()
+        .notNullable()
+        .defaultTo(knex.raw('uuid_generate_v4()'));
+      table
+        .string('route')
+        .unique()
+        .notNullable();
+      table.timestamps(true, true);
+    });
+
+exports.down = knex => knex.schema.dropTable('routes');
