0d848fb10edf68b63fc10322bd53c6fefa891e24
Jin827 <25231717+Jin827@users.noreply.github.com>
2019-11-10 20:12:34 +0900

    reverse

--- server/routes/mail.js
+++ server/routes/mail.js
@@ -15,7 +15,10 @@ module.exports = () => {
       .then(() =>
         res.status(201).send('Reserve Email has been sent successfully!'),
       )
-      .catch(err => res.status(400).json(err));
+      .catch(err => {
+        res.status(400).json(err);
+        next(err);
+      });
   });
 
   router.post('/bug', (req, res, next) => {
@@ -30,7 +33,10 @@ module.exports = () => {
 
     sendEmail(mailOptions)
       .then(() => res.status(201).send('Email has been sent successfully!'))
-      .catch(err => res.status(400).json(err));
+      .catch(err => {
+        res.status(400).json(err);
+        next(err);
+      });
   });
 
   return router;
