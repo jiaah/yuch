3f26c551d381b97a0ea1c28c5543f87e92a92592
Jin827 <25231717+Jin827@users.noreply.github.com>
2019-11-29 19:27:08 +0900

    welcome msg change

--- client/src/components/admin/userGuide/welcome.js
+++ client/src/components/admin/userGuide/welcome.js
@@ -4,14 +4,14 @@ import TextField from '@material-ui/core/TextField';
 const Welcome = ({ handleChange, adminCompanyName, state, textField }) => {
   const { username, password, mealPrice, email } = state;
   return (
-    <React.Fragment>
+    <div className="f-mini">
       <p className="mt4">
         {adminCompanyName}은 소중한 고객님께 더 나은 서비스를 제공하고자&#8201;
         <br />
         <span className="b">자동화된 시스템 </span>
         으로 운영합니다.
       </p>
-      <p className="mt3 lh-3">
+      <p className="mt4 lh-2">
         # {adminCompanyName} 홈페이지에서{' '}
         <span className="b c-blue">식수량 신청 또는 변경</span>
         <br /># 식수량이 일정하신 고객님을 위한{' '}
@@ -23,8 +23,8 @@ const Welcome = ({ handleChange, adminCompanyName, state, textField }) => {
         <br /># 지난 거래명세서{' '}
         <span className="b c-blue">언제, 어디서든 열람</span>
       </p>
-      <div className="flex flex-column-m items-center mt3">
-        <div className="text-br pb2">
+      <div className="flex flex-column-m items-center mh3">
+        <div className="pb2 text-br">
           <TextField
             name="username"
             label="아이디"
@@ -56,16 +56,12 @@ const Welcome = ({ handleChange, adminCompanyName, state, textField }) => {
         </div>
       </div>
       <p className="mt3 underline">https://yu-chung.com/</p>
-      <p className="mt2 f-mini">
-        검색창에서 &#39;
+      <p className="mt1 mb3">
+        <span className="b">&#39;구글 크롬&#39;</span> 웹 브라우저에서 &#39;
         <span className="b">유청 위탁급식</span>
-        &#39;을 입력하세요.
+        &#39;을 검색하세요.
       </p>
-      <p className="c-text2 f-xs">
-        &#42; 웹 브라우저는 &#39;인터넷 익스플로러&#39;가 아닌 &#39;구글
-        크롬&#39;을 이용해 주시기 바랍니다.
-      </p>
-    </React.Fragment>
+    </div>
   );
 };
 export default Welcome;
