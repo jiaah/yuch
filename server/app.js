b060e70a5d48794fe70e9e2dea4c09c116b13c6e
jiah-hm <jiah@hellomarket.com>
2020-08-10 02:05:23 +0900

    cors 제거

--- server/app.js
+++ server/app.js
@@ -13,37 +13,42 @@ Model.knex(knex);
 const app = express();
 
 // CORS Config
-const whitelist = ['https://yu-chung.com', 'http://localhost:9080/'];
+// const whitelist = ['https://yu-chung.com', 'http://localhost:9080/'];
 
-const msg =
-  'The CORS policy for this site does not allow access from the specified Origin.';
+// const msg =
+//   'The CORS policy for this site does not allow access from the specified Origin.';
 
-const corsOptions = {
-  origin(origin, callback) {
-    const originIsWhitelisted = whitelist.indexOf(origin) !== -1;
-    callback(new Error(msg), originIsWhitelisted);
-  },
-  credentials: true,
-  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-};
+// const corsOptions = {
+//   origin(origin, callback) {
+//     const originIsWhitelisted = whitelist.indexOf(origin) !== -1;
+//     callback(new Error(msg), originIsWhitelisted);
+//   },
+//   credentials: true,
+//   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+// };
 
-app.use(cors(corsOptions));
-// include before other routes
-app.options('*', cors(corsOptions));
+// app.use(cors(corsOptions));
+// // include before other routes
+// app.options('*', cors(corsOptions));
 
-// app.use((req, res, next) => {
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST', 'PATCH', 'DELETE');
-//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-// res.header('Access-Control-Allow-Credentials', true);
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Methods', 'GET, POST', 'PATCH', 'DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', true);
 
-//   if (req.method === 'OPTIONS') {
-//     res.header('Access-Control-Allow-Origin', req.headers.origin);
-//   } else {
-//     res.header('Access-Control-Allow-Origin', 'https://yu-chung.com');
-//   }
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+  } else {
+    const allowedOrigins = ['https://yu-chung.com', 'http://localhost:9080'];
+    const origin = req.headers.origin;
 
-//   return next();
-// });
+    if (allowedOrigins.indexOf(origin) > -1) {
+      res.header('Access-Control-Allow-Origin', origin);
+    }
+  }
+
+  return next();
+});
 
 app.use(logger('dev'));
 // extract POST data from HTTP request
