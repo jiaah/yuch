c3986f9c7adc8f54fa716aad3680cb2378e05727
jiah-hm <jiah@hellomarket.com>
2020-08-27 08:37:42 +0900

    fix typo

--- server/app.js
+++ server/app.js
@@ -33,7 +33,7 @@ const app = express();
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Methods', 'GET', 'POST', 'PATCH', 'DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Headers', 'Content-Type', 'Authorization');
   res.header('Access-Control-Allow-Credentials', true);
 
   if (req.method === 'OPTIONS') {
