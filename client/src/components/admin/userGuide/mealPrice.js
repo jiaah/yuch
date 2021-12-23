23fcf5af265203084c11566b1c93a5ae64b86b36
Jin827 <25231717+Jin827@users.noreply.github.com>
2021-12-24 07:43:33 +0900

    식수가격 인상 문구 수정

--- client/src/components/admin/userGuide/mealPrice.js
+++ client/src/components/admin/userGuide/mealPrice.js
@@ -15,7 +15,7 @@ const MealPrice = ({
     <React.Fragment>
       <p className="guide--margin">
         2018년 이후 식수가격을 동결하였으나 뛰는 물가와 인건비 상승으로
-        부득이하게 <span className="b c-blue">식수가격이 인상</span>
+        부득이하게 <span className="b c-blue">식수가격을 인상</span>
         하게 <br />
         됨을 알려드립니다.
       </p>
